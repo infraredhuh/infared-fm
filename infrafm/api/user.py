7d26033ec61763fa51a55aaf336c3483290386ba
infrared <faneorg.official@gmail.com>
2025-07-08 11:24:32 -0400

    Update user.py

--- infrafm/api/user.py
+++ infrafm/api/user.py
@@ -6,7 +6,7 @@ if TYPE_CHECKING:
     from ..client import LastFMClient
 
 class UserAPI:
-    def __init__(self, client: LastFMClient):
+    def __init__(self, client: "LastFMClient"):
         self._client = client
 
     async def get_info(self, user: str) -> User:

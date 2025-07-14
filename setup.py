c69dbad298a56becb54c21968f4f0c0f674b3ad8
infrared <faneorg.official@gmail.com>
2025-07-14 11:44:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 # we go dumm
 setup(
     name="infrared-fm",
-    version="1.6.0",
+    version="1.7.0",
     description="Last.FM wrapper by Infrared LLC.",
     author="infrared",
     author_email="faneorg.official@gmail.com",
